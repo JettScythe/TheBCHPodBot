7f924b85e91db1f545fad70cd9aac5cfbd340fff
JettScythe <jettscythe@hotmail.com>
2022-12-04 13:24:58 -0400

    fix(price.py): formats the 24hr change as percent
    also add example .env file

--- thebchpodbot/helpers/price.py
+++ thebchpodbot/helpers/price.py
@@ -10,10 +10,13 @@ def get_fiat_value(currencies: Optional[Union[List[str], str]] = None) -> Dict:
         currencies = 'usd'
     price = cg.get_price('bitcoin-cash', currencies, include_market_cap=True, include_24hr_vol=True, include_24hr_change=True)
     price = price["bitcoin-cash"]
-    """
-    # This works for most fiat, but not crypto pairs
     for k, v in price.items():
         currency = k.split("_")[0]
+        if k.endswith("change"):
+            v /= 100.
+            price[k] = "{:.2%}".format(v)
+    """
+    # This works for most fiat, but not crypto pairs
         if k.endswith("vol") or k.endswith("cap"):
             price[k] = format_currency(v, currency.upper()) """
     price = json.dumps(price, indent=2)
